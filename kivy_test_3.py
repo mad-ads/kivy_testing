02cc868bc7ca99f2daed66bc7ee65170fc1ebc68
mad-ads <39843888+mad-ads@users.noreply.github.com>
2018-06-01 23:01:12 -0400

    Screen Transition + Simple Paint

--- kivy_test_3.py
+++ kivy_test_3.py
@@ -1,6 +1,21 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
+from kivy.uix.widget import Widget
+from kivy.graphics import Line
+
+class DrawInput(Widget):
+    def on_touch_down(self, touch):
+        print(touch)
+        with self.canvas:
+            touch.ud['line'] = Line(points = (touch.x, touch.y))
+
+    def on_touch_move(self, touch):
+        print(touch)
+        touch.ud['line'].points += (touch.x, touch.y)
+
+    def on_touch_up(self, touch):
+        print('Release!', touch)
 
 class MainScreen(Screen):
     pass
